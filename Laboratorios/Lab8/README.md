114c9c898b78f8451e10cefc2398bf95a243681b
Damian Sandoval <will.ddr@gmail.com>
2023-05-16 23:08:38 -0300

    arreglamos ami y casi andan el provisioner

---
+++ Laboratorios/Lab8/README.md
@@ -0,0 +1,23 @@
+Instalamos Ubuntu 20.04 porque las otras amis se rompen!
+
+    Apache (httpd) (apache2 en ubuntu)
+        Se instala como dependencia al momento de instalar php5.6    
+
+
+    PHP5.6
+        comandos:
+            sudo apt update
+            sudo apt install -y software-properties-common
+            sudo add-apt-repository -y ppa:ondrej/php
+            sudo apt update
+            sudo apt install -y php5.6 php5.6-cli php5.6-common php5.6-mbstring php5.6-mysql php5.6-curl php5.6-gd php5.6-xml php5.6-zip
+
+
+    MySQL 5.6 o 5.7 (el cliente no anda de momento en Ubuntu)
+
+    Conector apache mysql (en Amazon linux se llama php-mysql y en Ubuntu tambien)
+        comandos:    
+            sudo apt install -y php-mysql
+
+Referencias:
+https://launchpad.net/~ondrej/+archive/ubuntu/php
--- Laboratorios/Lab8/instances.tf
+++ Laboratorios/Lab8/instances.tf
@@ -1,9 +1,23 @@
 resource "aws_instance" "webapp-server01" {
-  ami                    = "ami-02396cdd13e9a1257"
+  ami                    = "ami-0aa2b7722dc1b5612"
   instance_type          = "t2.large"
   key_name               = "vockey"
   vpc_security_group_ids = [aws_security_group.ssh-http-access.id]
   subnet_id              = aws_subnet.subnet1-practico-3tier.id
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    host        = self.public_ip
+    private_key = file("/home/damian/Downloads/vockey.pem")
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo apt update",
+      "sudo apt install -y software-properties-common",
+      "sudo add-apt-repository -y ppa:ondrej/php",
+      "sudo apt update",
+      "sudo apt install -y git php5.6 php5.6-cli php5.6-common php5.6-mbstring php5.6-mysql php5.6-curl php5.6-gd php5.6-xml php5.6-zip",
+    "sudo git clone https://github.com/mauricioamendola/simple-ecomme.git /tmp/"]
+  }
   tags = {
     Name = "webapp-server01"
   }
@@ -11,13 +25,40 @@ resource "aws_instance" "webapp-server01" {
 }
 
 resource "aws_instance" "webapp-server02" {
-  ami                    = "ami-02396cdd13e9a1257"
+  ami                    = "ami-0aa2b7722dc1b5612"
   instance_type          = "t2.large"
   key_name               = "vockey"
   vpc_security_group_ids = [aws_security_group.ssh-http-access.id]
   subnet_id              = aws_subnet.subnet2-practico-3tier.id
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    host        = self.public_ip
+    private_key = file("~/Downloads/vockey.pem")
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo apt update",
+      "sudo apt install -y software-properties-common",
+      "sudo add-apt-repository -y ppa:ondrej/php",
+      "sudo apt update",
+      "sudo apt install -y git php5.6 php5.6-cli php5.6-common php5.6-mbstring php5.6-mysql php5.6-curl php5.6-gd php5.6-xml php5.6-zip",
+    "sudo git clone https://github.com/mauricioamendola/simple-ecomme.git /tmp/"]
+  }
   tags = {
     Name = "webapp-server02"
   }
   depends_on = [aws_db_instance.db-practico-3tier]
 }
+
+resource "aws_instance" "prueba" {
+  ami                    = "ami-0aa2b7722dc1b5612"
+  instance_type          = "t2.large"
+  key_name               = "vockey"
+  vpc_security_group_ids = [aws_security_group.ssh-http-access.id]
+  subnet_id              = aws_subnet.subnet2-practico-3tier.id
+  tags = {
+    Name = "prueba"
+  }
+  depends_on = [aws_db_instance.db-practico-3tier]
+}
+
