8449a5b8786299056acd8a8605ead6b44f2c1874
Damian Sandoval <will.ddr@gmail.com>
2023-05-16 21:37:54 -0300

    terminamos con rds

--- Laboratorios/Lab8/db.tf
+++ Laboratorios/Lab8/db.tf
@@ -1,17 +1,18 @@
 # creamos la base de datos
 resource "aws_db_instance" "db-practico-3tier" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "dbuser123"
-  password             = "dbpass123"
-  parameter_group_name = "default.mysql5.7"
-  db_subnet_group_name = aws_db_subnet_group.db-subnet-practico-3tier.id
-  skip_final_snapshot  = true
-  #depends_on = [ aws_instance.webapp-server01, aws_instance.webapp-server02 ]
+  allocated_storage      = 10
+  db_name                = "mydb"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t3.micro"
+  username               = "dbuser123"
+  password               = "dbpass123"
+  parameter_group_name   = "default.mysql5.7"
+  db_subnet_group_name   = aws_db_subnet_group.db-subnet-practico-3tier.id
+  vpc_security_group_ids = [aws_security_group.mysql_sg.id]
+  skip_final_snapshot    = true
 }
+
 resource "aws_db_subnet_group" "db-subnet-practico-3tier" {
   name       = "db-subnet-group"
   subnet_ids = [aws_subnet.subnet1-practico-3tier.id, aws_subnet.subnet2-practico-3tier.id]
--- Laboratorios/Lab8/sg.tf
+++ Laboratorios/Lab8/sg.tf
@@ -22,3 +22,22 @@ resource "aws_security_group" "ssh-http-access" {
   }
 
 }
+# creo el security group para poder acceder a la base de datos desde los webservers
+resource "aws_security_group" "mysql_sg" {
+  name   = "mysql-sg"
+  vpc_id = aws_vpc.vpc-practico-3tier.id
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    #cidr_blocks = [aws_subnet.subnet1-practico-3tier.cidr_block, aws_subnet.subnet2-practico-3tier.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
